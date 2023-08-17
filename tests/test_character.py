dcf012a7edd61e22dec4488028518a0791165e66
make bootstrap <avruchi@gmail.com>
2023-08-17 17:29:41 +0000

    test_character_update

--- tests/test_character.py
+++ tests/test_character.py
@@ -3,6 +3,25 @@ from levelup.character import Character
 
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
+        ARBITRARY_NAME = ""
+        testobj = Character(ARBITRARY_NAME)
+        self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_string(self):
         ARBITRARY_NAME = "Bob"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_getName(self):
+        ARBITRARY_NAME = "Uncle"
+        testobj = Character(ARBITRARY_NAME)
+        self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_enterMap(self):
+        ENTERED_MAP = True
+        testobj = Character(ENTERED_MAP)
+        self.assertEqual(ENTERED_MAP, testobj.name)
+    
+
+
+    
