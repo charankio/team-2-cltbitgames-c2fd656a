fd84ae5588227c07fca31fa0b7d3a7d9b6ed1394
sleepingturtle <josh@asleepingturtle.com>
2023-08-18 13:32:19 +0000

    gameMap test update

--- src/levelup/map.py
+++ src/levelup/map.py
@@ -3,13 +3,24 @@ import logging
 class GameMap:
     
     def __init__(self):
-        pass
+        numPositions = 100
 
     def getPositions(self):
         return [1, "a"]
 
-    @staticmethod
     def calculatePosition(startingPosition: dict, moveDirection: str):
+        moveDirection = moveDirection.lower()
+        newPosition = []
+    
+        if moveDirection == "f":
+            startingPosition[1] 
+        elif moveDirection == "b":
+            pass
+        elif moveDirection == "l":
+            pass
+        elif moveDirection == "r":
+            pass
+
         return [1, "b"]
 
     @staticmethod
--- tests/test_game_map.py
+++ tests/test_game_map.py
@@ -3,10 +3,29 @@ from levelup.map import GameMap
 
 
 class TestGetPosition(TestCase):
-    def test_calculatePosition(self):
-        testObj = GameMap.calculatePosition([1, "b"], "F")
-        self.assertEqual(testObj, [1, "b"])
+    def test_calculatePosition_forward(self):
+        initalPosition = [3, "f"]
+        moveDirection = "F"
+        testObj = GameMap.calculatePosition(initalPosition, moveDirection)
+        self.assertEqual(testObj, [1, "e"])
+
+    def test_calculatePosition_back(self):
+        initalPosition = [3, "f"]
+        moveDirection = "B"
+        testObj = GameMap.calculatePosition(initalPosition, moveDirection)
+        self.assertEqual(testObj, [1, "g"])
+
+    def test_calculatePosition_left(self):
+        initalPosition = [3, "f"]
+        moveDirection = "L"
+        testObj = GameMap.calculatePosition(initalPosition, moveDirection)
+        self.assertEqual(testObj, [2, "f"])
+
+    def test_calculatePosition_right(self):
+        initalPosition = [3, "f"]
+        moveDirection = "R"
+        testObj = GameMap.calculatePosition(initalPosition, moveDirection)
+        self.assertEqual(testObj, [4, "f"])
 
     def test_isPositionValid(self):
-        testObj = GameMap.isPositionValid([3, "f"])
-        assert testObj == False
+        pass
